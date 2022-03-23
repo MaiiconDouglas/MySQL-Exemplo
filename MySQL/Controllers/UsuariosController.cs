19155b7b639ebcda98354fab674cb5db72ff4d7c
0220832111034 <0220832111034@LAB4TC20>
2022-03-23 09:50:57 -0300

    Adicionar arquivos de projeto.

---
+++ MySQL/Controllers/UsuariosController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySQL.Entidades;
+using System.Linq;
+
+namespace MySQL.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Contexto db;
+        public UsuariosController(Contexto _db)
+        {
+            db = _db;
+        }
+        public ActionResult Index()
+        {
+            return View(    db.USUARIOS.ToList()    );
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Usuario dadosQueVemTela)
+        {
+            db.USUARIOS.Add(dadosQueVemTela);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return View(db.USUARIOS.Where(a => a.Id == id).FirstOrDefault());
+           
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(int id, Usuario dadosQueVemTela)
+        {
+            db.USUARIOS.Update(dadosQueVemTela);
+            db.SaveChanges();
+            return RedirectToAction("Index"); ;
+        }
+
+        // GET: UsuariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            db.USUARIOS.Remove(
+               db.USUARIOS.Where(a => a.Id == id).FirstOrDefault()
+                );
+            db.SaveChanges();
+            return RedirectToAction("index");
+            
+
+        }
+
+
+    }
+}
